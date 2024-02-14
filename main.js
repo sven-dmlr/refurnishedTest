a6d7924733cdb60f429601a295f85d52bf623bc8
Sven <sd@maui.d0l>
2024-02-15 00:10:55 +0100

    path fixed

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //IMPORT MODULES
 import "./style.css";
-import * as THREE from "/node_modules/three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import * as THREE from "three";
+import { OrbitControls } from "/node_modules/three/examples/jsm/controls/OrbitControls.js";
 import { TransformControls } from "three/addons/controls/TransformControls.js";
 import { STLExporter } from "three/addons/exporters/STLExporter.js";
 import GUI from "lil-gui";
